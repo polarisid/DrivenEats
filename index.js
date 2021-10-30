22b19c673bd5d446925ac0569ffee837c5b8e11c
polarisid <danielcaevalhorst@gmail.com>
2021-10-30 20:59:35 -0300

    Selecção individual por seção não a impede a seleção de outra seção-  bug ainda pois ao clicar em cima de um já selecionado ele não desabilita. Implementado opção para aprecer botão futuro

--- index.js
+++ index.js
@@ -1,18 +1,48 @@
 function select(elemento){
-    const elemento_selecionado = document.querySelector(".selecionado");
-    const icon_selected = document.querySelector(".mostrar")
-    console.log(elemento)
 
-    if(elemento_selecionado!==null){
+    if(elemento.querySelector(".almoco")!==null){
+        const elemento_selecionado = document.querySelector(".selecionado");
+        const icon_selected = document.querySelector(".mostrar")
+        if(elemento_selecionado!==null){
         elemento_selecionado.classList.toggle("selecionado");
-        console.log(elemento_selecionado)
-    }
-    if(icon_selected!==null){
+        }
+        if(icon_selected!==null){
         icon_selected.classList.toggle("mostrar");
+        }
+        elemento.querySelector(".icon-check").classList.add("mostrar");
+        elemento.classList.add("selecionado");
     }
-    elemento.querySelector(".icon-check").classList.add("mostrar");
-    elemento.classList.add("selecionado");
 
+    if(elemento.querySelector(".bebida")!==null){
+        const elemento_selecionado = document.querySelector(".bebida-section .selecionado");
+        const icon_selected = document.querySelector(".bebida-section .mostrar")
+        if(elemento_selecionado!==null){
+        elemento_selecionado.classList.toggle("selecionado");
+        }
+        if(icon_selected!==null){
+        icon_selected.classList.toggle("mostrar");
+        }
+        elemento.querySelector(".icon-check").classList.add("mostrar");
+        elemento.classList.add("selecionado");
+    }
+
+    if(elemento.querySelector(".sobremesa")!==null){
+        const elemento_selecionado = document.querySelector(".sobremesa-section .selecionado");
+        const icon_selected = document.querySelector(".sobremesa-section .mostrar")
+        if(elemento_selecionado!==null){
+        elemento_selecionado.classList.toggle("selecionado");
+        }
+        if(icon_selected!==null){
+        icon_selected.classList.toggle("mostrar");
+        }
+        elemento.querySelector(".icon-check").classList.add("mostrar");
+        elemento.classList.add("selecionado");
+    }
+    
+    if((document.querySelector(".sobremesa-section .selecionado")!==null) && (document.querySelector(".bebida-section .selecionado")!==null) && (document.querySelector(".prato-section .selecionado")!==null)){
+        console.log("Miseravao")
+    }
 }
+
 
 
