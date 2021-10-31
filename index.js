71e7fbc6d0f91d300c4d11473dc477e34d963e3b
polarisid <danielcaevalhorst@gmail.com>
2021-10-31 17:46:44 -0300

    Botões ok falta os bonus e enviar para o Whatsaspp

--- index.js
+++ index.js
@@ -41,7 +41,6 @@ function select(elemento){
     }
     
     if((document.querySelector(".sobremesa-section .selecionado")!==null) && (document.querySelector(".bebida-section .selecionado")!==null) && (document.querySelector(".prato-section .selecionado")!==null)){
-        console.log("Miseravao")
         const meubut = document.querySelector(".fixed-bar-container button");
         meubut.toggleAttribute("disabled")
         meubut.innerHTML ="Fechar Pedido";
@@ -52,3 +51,22 @@ function select(elemento){
 
 
 
+function fechar(){
+    //calcular valores selecionados
+    var valor_prato1 =[];
+    var valor_prato2 =[];
+    var valor_prato3 =[];
+    var valor_prato1 = document.querySelector(".prato-section .selecionado .valor").innerHTML.split(" ");
+    var valor_prato2 = document.querySelector(".bebida-section .selecionado .valor").innerHTML.split(" ");
+    var valor_prato3 = document.querySelector(".sobremesa-section .selecionado .valor").innerHTML.split(" ");
+    var valor_total = (parseFloat(valor_prato1[1].replace(",", ".")) + parseFloat(valor_prato2[1].replace(",", ".")) +parseFloat(valor_prato3[1].replace(",", "."))) ;
+    
+    var nome = window.prompt("Por favor insira seu nome completo", "nome");
+    var adress = window.prompt("Por favor insira seu Endereço", "Endereço");
+
+
+    console.log(valor_total.toFixed(2))
+    console.log(valor_prato1)
+
+
+}
