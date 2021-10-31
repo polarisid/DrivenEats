4390510e4326be43c94fb1479fa511518efa8c0e
polarisid <danielcaevalhorst@gmail.com>
2021-10-31 00:52:36 -0300

    Seleção individual ok, seleção por seção ok, interface tudo ok,botão finalizar estilizado, falta implementar a função do zap e somar os valores e adicionar mais itens

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 function select(elemento){
 
     if(elemento.querySelector(".almoco")!==null){
-        const elemento_selecionado = document.querySelector(".selecionado");
-        const icon_selected = document.querySelector(".mostrar")
+        const elemento_selecionado = document.querySelector(".prato-section .selecionado");
+        const icon_selected = document.querySelector(".prato-section .mostrar")
         if(elemento_selecionado!==null){
         elemento_selecionado.classList.toggle("selecionado");
         }
         if(icon_selected!==null){
         icon_selected.classList.toggle("mostrar");
         }
+        //para fazer toogle ao clicar novamente adicionar else aqui
         elemento.querySelector(".icon-check").classList.add("mostrar");
         elemento.classList.add("selecionado");
     }
@@ -41,6 +42,11 @@ function select(elemento){
     
     if((document.querySelector(".sobremesa-section .selecionado")!==null) && (document.querySelector(".bebida-section .selecionado")!==null) && (document.querySelector(".prato-section .selecionado")!==null)){
         console.log("Miseravao")
+        const meubut = document.querySelector(".fixed-bar-container button");
+        meubut.toggleAttribute("disabled")
+        meubut.innerHTML ="Fechar Pedido";
+        meubut.classList.add("fechar-pedido");
+
     }
 }
 
